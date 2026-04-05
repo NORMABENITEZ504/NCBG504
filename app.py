15942513737e0ea51e54bda0763bd8b30beda918
NORMABENITEZ504 <normaceciliabenitez3@gmail.com>
2026-04-05 14:40:40 -0600

    app.py

--- app.py
+++ app.py
@@ -3,23 +3,26 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import pandas as pd
 
+# Configuración visual (Debe ir al principio)
+st.set_page_config(page_title="LISA Real-Time Stats", layout="wide")
+
 # TUS CÓDIGOS REALES
 CLIENT_ID = 'f693630ca5df44fa8f10bbcd5fbc6830'
 CLIENT_SECRET = '9f90223ed60f46d2b5f39d3a1eb06c2e'
 
-# Conexión con Spotify
-auth_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
-sp = spotipy.Spotify(auth_manager=auth_manager)
-
-st.set_page_config(page_title="LISA Real-Time Stats", layout="wide")
-st.title("🤳 LISA Discography Tracker")
-
-# ID de Lisa
-LISA_URI = 'spotify:artist:5L1oOat9Y8mYvRsmVOSI0O'
-
 try:
-    # Traer canciones top de Lisa
-    results = sp.artist_top_tracks(LISA_URI)
+    # Conexión con Spotify
+    auth_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+    sp = spotipy.Spotify(auth_manager=auth_manager)
+
+    st.title("🤳 LISA Discography Tracker")
+
+    # ID de Lisa
+    LISA_URI = 'spotify:artist:5L1oOat9Y8mYvRsmVOSI0O'
+
+    # --- EL ERROR ESTABA AQUÍ ---
+    # Se debe agregar country='HN' (Honduras) para que Spotify sepa qué mercado consultar
+    results = sp.artist_top_tracks(LISA_URI, country='HN')
     tracks = results['tracks']
 
     data = []
@@ -35,6 +38,7 @@ try:
     # Mostrar métricas
     col1, col2 = st.columns(2)
     with col1:
+        # Nota: Este dato de posición es manual, se puede automatizar después
         st.metric("Top en Honduras 🇭🇳", "Rockstar", "Puesto #34")
     with col2:
         st.metric("Más Popular hoy", df.iloc[0]['Canción'], f"{df.iloc[0]['Popularidad']}/100")
